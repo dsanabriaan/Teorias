6b2251860de014621ac16ca5747fc9ffd87c1f34
dsanabriaan <daniel.sanabria90@gmail.com>
2018-02-24 02:11:33 -0500

    cambio de exp reg

--- TallerTC/src/tallertc/TallerTC.java
+++ TallerTC/src/tallertc/TallerTC.java
@@ -5,13 +5,13 @@ import  java.util.regex.Pattern;
 import  java.util.regex.Matcher;
 public class TallerTC {
     static  boolean validar (String input){
-        Pattern p = Pattern.compile("^((\\\\d{2})(\\\\d+.+)(\\\\d{2}))$");
+        Pattern p = Pattern.compile("^28(\\\\d*)[^90]$");
         Matcher m = p.matcher(input);
         return m.matches();
     }
     
     public static void main(String[] args) {
-        System.err.println(validar("2511125"));
+        System.err.println(validar("28888888888891"));
         
     }
     
