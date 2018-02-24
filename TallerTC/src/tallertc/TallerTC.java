7b3e1916a94512b0cb53f688d975d25abfb4d6e4
dsanabriaan <daniel.sanabria90@gmail.com>
2018-02-24 02:43:51 -0500

    cambio de exp reg2

--- TallerTC/src/tallertc/TallerTC.java
+++ TallerTC/src/tallertc/TallerTC.java
@@ -5,13 +5,13 @@ import  java.util.regex.Pattern;
 import  java.util.regex.Matcher;
 public class TallerTC {
     static  boolean validar (String input){
-        Pattern p = Pattern.compile("^28(\\\\d*)[^90]$");
+        Pattern p = Pattern.compile("^28(\\d*)[^90]$");
         Matcher m = p.matcher(input);
         return m.matches();
     }
     
     public static void main(String[] args) {
-        System.err.println(validar("28888888888891"));
+        System.err.println(validar("28444449"));
         
     }
     
